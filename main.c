62aff4a3b80340d9d297791352b8dbecfe116ff9
Mesut Tunçer <53650821+massout@users.noreply.github.com>
2021-03-25 15:39:15 +0300

    Update main.c

--- main.c
+++ main.c
@@ -68,9 +68,11 @@ void activate (GtkApplication *app, gpointer user_data){
 }
 
 
-void main(int argc, char **argv){
+int main(int argc, char **argv){
     GtkApplication *app = gtk_application_new ("com.massout.passlet", G_APPLICATION_FLAGS_NONE);
+
     g_signal_connect (app, "activate", G_CALLBACK (activate), NULL);
-    g_application_run (G_APPLICATION (app), argc, argv);
-    g_object_unref (app);
+
+    return g_application_run (G_APPLICATION (app), argc, argv);
 }
+
